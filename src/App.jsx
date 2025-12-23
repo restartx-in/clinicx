e5dc3579e9be04786639eff25a4bbb7af9a7f259
Fasil2526 <fasilkooliyodan2526@gmail.com>
2025-12-23 23:29:10 +0530

    build failed issue fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import { Navbar } from '@/components/NavbarSection/Navbar';
-import { Footer } from '@/components/FooterSection/Footer';
-import { Home } from '@/pages/HomePage/Home';
-import { About } from '@/pages/AboutPage/About';
-import { DesignerCollective } from '@/pages/DesignerCollectivePage/DesignerCollective';
-import { GlobalModels } from '@/pages/GlobalModelsPage/GlobalModels';
-import { RunwayAcademy } from '@/pages/RunwayAcademyPage/RunwayAcademy';
-import { Apply } from '@/pages/ApplyPage/Apply';
-import { PageRoute } from '@/constants/types';
-import './index.scss';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import { Navbar } from "@/components/NavbarSection/Navbar";
+import { Footer } from "@/components/FooterSection/Footer";
+import { Home } from "@/pages/HomePage/Home";
+import { About } from "@/pages/AboutPage/About";
+import { DesignerCollective } from "@/pages/DesignerCollectivePage/DesignerCollective";
+import { GlobalModels } from "@/pages/GlobalModelsPage/GlobalModels";
+import { RunwayAcademy } from "@/pages/RunwayAcademyPage/RunwayAcademy";
+import { Apply } from "@/pages/ApplyPage/Apply";
+import { PageRoute } from "@/constants/types";
+import "./index.scss";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
-  
+
   React.useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
-  
+
   return null;
 };
 
@@ -33,13 +38,16 @@ const MainContent = () => {
         <Routes>
           <Route path={PageRoute.HOME} element={<Home />} />
           <Route path={PageRoute.ABOUT} element={<About />} />
-          <Route path={PageRoute.DESIGNER_COLLECTIVE} element={<DesignerCollective />} />
+          <Route
+            path={PageRoute.DESIGNER_COLLECTIVE}
+            element={<DesignerCollective />}
+          />
           <Route path={PageRoute.GLOBAL_MODELS} element={<GlobalModels />} />
           <Route path={PageRoute.RUNWAY_ACADEMY} element={<RunwayAcademy />} />
           <Route path={PageRoute.APPLY} element={<Apply />} />
         </Routes>
       </main>
-      
+
       {/* Pass the new prop to Footer. Footer is always rendered. */}
       <Footer hideBrandColumn={hideBrandColumn} />
     </div>
