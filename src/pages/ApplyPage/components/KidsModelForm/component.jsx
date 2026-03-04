0adf5de117d183d03769684b5ad23744fedfe00c
shafinmuhammad <shafin01123@gmail.com>
2026-03-04 10:56:44 +0530

    updated kids modal for gender update

--- src/pages/ApplyPage/components/KidsModelForm/component.jsx
+++ src/pages/ApplyPage/components/KidsModelForm/component.jsx
@@ -94,8 +94,8 @@ const KidsModelForm = () => {
             <option value="" disabled>
               Select Gender
             </option>
-            <option value="Male">Male</option>
-            <option value="Female">Female</option>
+            <option value="Boy">Boy</option>
+            <option value="Girl">Girl</option>
             <option value="Other">Other</option>
           </select>
         </div>
